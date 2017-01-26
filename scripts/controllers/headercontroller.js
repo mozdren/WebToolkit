042fb78f1f2b92ad88fd8859d43fafcdd65954a0
Karel Mozdren <mozdren@gmail.com>
2017-01-26 17:05:10 +0100

    json basics

--- scripts/controllers/headercontroller.js
+++ scripts/controllers/headercontroller.js
@@ -1,6 +1,22 @@
-headerApplication.controller("headerController", function($scope) {
+headerApplication.controller("headerController", function($scope, $http) {
+    
+    /// Model
+    
     $scope.background = "images/office.jpg";
     $scope.title = "Lorem ipsum dolor";
     $scope.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pulvinar sem non libero vehicula, nec placerat elit ullamcorper. Quisque a congue lacus. Nam porta massa ex, nec auctor arcu ultrices ac. Morbi non ultricies nibh.";
-    $scope.style = "background-image: url(" + $scope.background + ")"
+
+    /// Computed
+
+    $scope.style = function () {
+        return "background-image: url(" + $scope.background + ")";
+    }
+
+    /// Methods
+
+    /*$http.get("data/header.json").then(function(response) {
+        $scope.background = response.data.background;
+        $scope.title = response.data.title;
+        $scope.text = response.data.text;
+    });*/
 });
